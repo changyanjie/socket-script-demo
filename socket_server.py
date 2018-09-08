6bf3ad4374bfa3fbe889077c4186df035f1f3f05
yanjie.chang <yanjie.chang@ele.me>
2018-09-09 01:37:24 +0800

    up

--- socket_server.py
+++ socket_server.py
@@ -3,6 +3,7 @@ import __future__
 import socket
 import sys
 import six
+import time
 from flask import Flask
 from flask_socketio import SocketIO, send, emit
 from prettytable import PrettyTable
@@ -35,6 +36,8 @@ def handle_agent_retailer(json):
     event = 'agent_retailer'
     retailer_id = json['retailer_id']
     send_msg(event, u'查询商户信息:', agent_retailer.search_retailer_info(retailer_id))
+    # 测试分段输出，sleep 3s
+    time.sleep(3)
     send_msg(event, u'当前主承运商:', agent_retailer.search_current_agents(retailer_id))
 
 
